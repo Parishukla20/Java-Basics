81928132f93051f357a54bc73795e14e2c8d93da
Pari Shukla <144891407+Parishukla20@users.noreply.github.com>
2026-02-12 15:02:46 +0530

    Add files via upload

---
+++ Three.java
@@ -0,0 +1,20 @@
+//Fibonacci Series using iteration
+
+import java.util.Scanner;
+
+public class Three {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int first = 0;
+        int second =1;
+        System.out.println(first);
+        System.out.println(second);
+        for(int i = 0;i<=n;i++){
+            int next = first + second;
+            System.out.println(next);
+            first = second;
+            second = next;
+        }
+    }
+}
