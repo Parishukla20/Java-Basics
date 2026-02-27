dfd3e59bc1e94b88cf821565e059dc742d923dc2
Pari Shukla <144891407+Parishukla20@users.noreply.github.com>
2026-02-27 14:55:54 +0530

    Add files via upload

---
+++ Six.java
@@ -0,0 +1,17 @@
+//Character frequency in string by using hashmap
+import java.util.*;
+public class Six {
+    public static void main(String args[]){
+        String str = "PARI PRERNA";
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i =0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(map.containsKey(ch)){
+                map.put(ch, map.get(ch) + 1);
+            }else {
+                map.put(ch, 1);
+            }
+            System.out.println(map);
+        }
+    }
+}
