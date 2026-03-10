debb5d3887bf4430076e3ce903969182ff112725
Pari Shukla <144891407+Parishukla20@users.noreply.github.com>
2026-03-10 18:08:58 +0530

    Add files via upload

---
+++ Seven.java
@@ -0,0 +1,9 @@
+//Reverse a string 
+public class Seven {
+    public static void main(String args[]){
+        String str = "Delhi";
+        for(int i =str.length()-1;i>=0;i--){
+            System.out.print(str.charAt(i));
+        }
+    }
+}
