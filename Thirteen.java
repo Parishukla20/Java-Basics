4336a264170d871f56d0680ad81ca00a3b4c67c4
Pari Shukla <144891407+Parishukla20@users.noreply.github.com>
2026-06-03 19:50:31 +0530

    Create Thirteen.java

---
+++ Thirteen.java
@@ -0,0 +1,37 @@
+public class TwentyFour {
+    public static void main(String[] args) {
+        int n = 5;
+
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+
+            // Spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Stars
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+
+            // Spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Stars
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
